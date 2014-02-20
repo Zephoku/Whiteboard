2ef6c9418309204d6664235939ba90c8643e5523
fahadnathani <fahadnathani@gmail.com>
2014-02-20 13:48:20 -0800

    Added Zoom Functionality and Buttons

--- www/js/whiteboard.js
+++ www/js/whiteboard.js
@@ -1,4 +1,7 @@
 var canvas;
+var SCALE_FACTOR = 0.5;
+var canvasScale = 1;
+
 $( document ).ready( function() {
   // Initialize Drawing Canvas
   canvas = new fabric.Canvas('whiteboard');
@@ -12,4 +15,109 @@ $( document ).ready( function() {
 
 function clearFun() {
 	canvas.clear();
+}
+
+function zoomOutFun() {
+	var objects = canvas.getObjects();
+	    for (var i in objects) {
+	        var scaleX = objects[i].scaleX;
+	        var scaleY = objects[i].scaleY;
+	        var left = objects[i].left;
+	        var top = objects[i].top;
+	        
+	        var tempScaleX = scaleX * SCALE_FACTOR;
+	        var tempScaleY = scaleY * SCALE_FACTOR;
+	        var tempLeft = left * SCALE_FACTOR;
+	        var tempTop = top * SCALE_FACTOR;
+	        
+	        objects[i].scaleX = tempScaleX;
+	        objects[i].scaleY = tempScaleY;
+	        objects[i].left = tempLeft;
+	        objects[i].top = tempTop;
+	        
+	        objects[i].setCoords();
+	    }
+
+	canvas.renderAll();
+	canvasScale *= SCALE_FACTOR;
+}
+
+function zoomInFun() {
+	var objects = canvas.getObjects();
+	    for (var i in objects) {
+	        var scaleX = objects[i].scaleX;
+	        var scaleY = objects[i].scaleY;
+	        var left = objects[i].left;
+	        var top = objects[i].top;
+	        
+	        var tempScaleX = scaleX / SCALE_FACTOR;
+	        var tempScaleY = scaleY / SCALE_FACTOR;
+	        var tempLeft = left / SCALE_FACTOR;
+	        var tempTop = top / SCALE_FACTOR;
+	        
+	        objects[i].scaleX = tempScaleX;
+	        objects[i].scaleY = tempScaleY;
+	        objects[i].left = tempLeft;
+	        objects[i].top = tempTop;
+	        
+	        objects[i].setCoords();
+	    }
+
+	canvas.renderAll();
+	canvasScale /= SCALE_FACTOR;
+
+}
+
+
+function resetZoomToDefault() {
+
+	var objects = canvas.getObjects();
+    for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+    
+        var tempScaleX = scaleX * (1 / canvasScale);
+        var tempScaleY = scaleY * (1 / canvasScale);
+        var tempLeft = left * (1 / canvasScale);
+        var tempTop = top * (1 / canvasScale);
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+        
+    canvas.renderAll();
+
+}
+
+function resetDefaultToZoom() {
+
+	var objects = canvas.getObjects();
+    for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+    
+        var tempScaleX = scaleX * (canvasScale);
+        var tempScaleY = scaleY * (canvasScale);
+        var tempLeft = left * (canvasScale);
+        var tempTop = top * (canvasScale);
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+        
+    canvas.renderAll();
+
+
 }
